e3eb8fea313009e2a8d4b05010b88dc0f7ba23ac
GraziTDS <113216820+GraziTDS@users.noreply.github.com>
2026-09-22 20:52:59 -0300

    feat: adicionar TarefaRepository e TarefaViewModel

---
+++ app/src/androidTest/java/grazitds/com/example/to_do_list/repository/TarefaRepository.kt
@@ -0,0 +1,16 @@
+package grazitds.com.example.to_do_list.repository
+
+import grazitds.com.example.to_do_list.data.Tarefa
+import grazitds.com.example.to_do_list.data.TarefaDao
+import kotlinx.coroutines.flow.Flow
+
+class TarefaRepository(private val dao: TarefaDao) {
+
+    val tarefas: Flow<List<Tarefa>> = dao.listarTodas()
+
+    suspend fun inserir(tarefa: Tarefa) = dao.inserir(tarefa)
+
+    suspend fun atualizar(tarefa: Tarefa) = dao.atualizar(tarefa)
+
+    suspend fun deletar(tarefa: Tarefa) = dao.deletar(tarefa)
+}
